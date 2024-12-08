5705fca973ea3f4bb6eb2e24fad93f6bb5155377
origin-coding <1604070358@qq.com>
2024-12-08 21:59:02 +0800

    feat: 添加重载；添加新的校验方法；添加注释

--- src/main/kotlin/CharacterOptions.kt
+++ src/main/kotlin/CharacterOptions.kt
@@ -1,12 +1,25 @@
 package com.origincoding
 
+/**
+ * 生成密码时的字符集选项
+ *
+ * @property uppercase 是否包含大写字母
+ * @property lowercase 是否包含小写字母
+ * @property digits 是否包含数字
+ * @property specialChars 是否包含特殊字符
+ */
 data class CharacterOptions(
     val uppercase: Boolean = true,
     val lowercase: Boolean = true,
     val digits: Boolean = true,
     val specialChars: Boolean = true
 ) {
-    fun characterTypes(): Int {
-        return arrayOf(specialChars, uppercase, lowercase, digits).count { it }
+    /**
+     * 字符集类型的数量
+     *
+     * @return Int 字符集类型的数量
+     */
+    val characterTypes: Int by lazy {
+        arrayOf(specialChars, uppercase, lowercase, digits).count { it }
     }
 }
--- src/main/kotlin/PasswordGenerator.kt
+++ src/main/kotlin/PasswordGenerator.kt
@@ -1,51 +1,120 @@
+@file:Suppress("Unused")
+
 package com.origincoding
 
 import org.apache.commons.text.RandomStringGenerator
 
-fun generatePassword(
-    minLength: Int = 8,
-    maxLength: Int = 16,
-    characterOptions: CharacterOptions = CharacterOptions(),
-    count: Int = 1
-): List<String> {
+inline fun <T> MutableList<T>.addAllIf(
+    vararg element: T,
+    condition: () -> Boolean
+) {
+    if (condition()) {
+        this.addAll(element)
+    }
+}
+
+/**
+ * 生成密码
+ *
+ * @param minLength 密码最小长度
+ * @param maxLength 密码最大长度
+ * @param characterOptions 字符集选项
+ * @param count 生成密码数量
+ *
+ * @see CharacterOptions
+ */
+internal fun buildGenerator(
+    minLength: Int, maxLength: Int, characterOptions: CharacterOptions, count: Int
+): RandomStringGenerator {
     // 对密码长度进行校验
     require(minLength > 3) { "最小长度不能小于4！" }
     require(maxLength < 33) { "最大长度不能大于32！" }
     require(minLength <= maxLength) { "最大长度不能小于最小长度！" }
 
     // 对字符集类型进行校验
-    require(characterOptions.characterTypes() > 1) { "至少选择两种类型的字符！" }
+    require(characterOptions.characterTypes > 1) { "至少选择两种类型的字符！" }
 
     // 对生成的密码数量进行校验
     require(count > 0) { "密码数量不能小于0！" }
 
+    // 确定使用的字符集
     val charArrays = mutableListOf<CharArray>()
-    if (characterOptions.uppercase) {
-        charArrays.add(charArrayOf('A', 'Z'))
-    }
-    if (characterOptions.lowercase) {
-        charArrays.add(charArrayOf('a', 'z'))
-    }
-    if (characterOptions.digits) {
-        charArrays.add(charArrayOf('0', '9'))
-    }
-    if (characterOptions.specialChars) {
-        charArrays.add(charArrayOf('!', '!'))
-        charArrays.add(charArrayOf('#', '&'))
-        charArrays.add(charArrayOf('*', '/'))
-        charArrays.add(charArrayOf(':', '@'))
-        charArrays.add(charArrayOf('^', '^'))
-        charArrays.add(charArrayOf('|', '|'))
-        charArrays.add(charArrayOf('~', '~'))
+    charArrays.addAllIf(charArrayOf('a', 'z')) { characterOptions.lowercase }
+    charArrays.addAllIf(charArrayOf('A', 'Z')) { characterOptions.uppercase }
+    charArrays.addAllIf(charArrayOf('0', '9')) { characterOptions.digits }
+    charArrays.addAllIf(
+        charArrayOf('!', '!'),
+        charArrayOf('#', '&'),
+        charArrayOf('*', '/'),
+        charArrayOf(':', '@'),
+        charArrayOf('^', '^'),
+        charArrayOf('|', '|'),
+        charArrayOf('~', '~'),
+    ) {
+        characterOptions.specialChars
     }
 
-    val generator = RandomStringGenerator.builder().withinRange(*charArrays.toTypedArray()).get()
+    // 构造Generator，生成密码
+    return RandomStringGenerator.builder().withinRange(*charArrays.toTypedArray<CharArray>()).get()
+}
 
-    val list = mutableListOf<String>()
-    for (i in 1..count) {
-        val password = generator.generate(minLength, maxLength)
-        list.add(password)
+/**
+ * 对生成的密码进行校验，在characterOptions中允许的字符集要在生成的密码中出现
+ *
+ * @param password 生成的密码
+ * @param characterOptions 字符集选项
+ */
+internal fun passwordIsValid(password: String, characterOptions: CharacterOptions): Boolean {
+    return password.let {
+        (it.any { char -> char.isUpperCase() } || !characterOptions.uppercase) &&
+                (it.any { char -> char.isLowerCase() } || !characterOptions.lowercase) &&
+                (it.any { char -> char.isDigit() } || !characterOptions.digits) &&
+                (it.any { char -> !char.isLetterOrDigit() } || !characterOptions.specialChars)
     }
+}
 
-    return list
+/**
+ * 生成多个密码
+ *
+ * @param minLength 密码最小长度，默认为8
+ * @param maxLength 密码最大长度，默认为16
+ * @param characterOptions 字符集选项
+ * @param count 生成密码数量
+ *
+ * @return 生成的密码列表
+ *
+ * @see CharacterOptions
+ */
+fun generatePassword(
+    minLength: Int = 8, maxLength: Int = 16, characterOptions: CharacterOptions = CharacterOptions(), count: Int = 1
+): List<String> {
+    val generator = buildGenerator(minLength, maxLength, characterOptions, count)
+
+    return generateSequence {
+        generator.generate(minLength, maxLength)
+    }.filter {
+        passwordIsValid(it, characterOptions)
+    }.take(count).toList()
+}
+
+/**
+ * 生成单个密码
+ *
+ * @param minLength 密码最小长度，默认为8
+ * @param maxLength 密码最大长度，默认为16
+ * @param characterOptions 字符集选项
+ *
+ * @return 生成的密码
+ *
+ * @see CharacterOptions
+ */
+fun generatePassword(
+    minLength: Int = 8, maxLength: Int = 16, characterOptions: CharacterOptions = CharacterOptions()
+): String {
+    val generator = buildGenerator(minLength, maxLength, characterOptions, 1)
+    return generateSequence {
+        generator.generate(minLength, maxLength)
+    }.filter {
+        passwordIsValid(it, characterOptions)
+    }.first()
 }
